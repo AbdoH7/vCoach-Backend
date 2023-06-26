3a361ad345b356f2576a380840d11fd93c5d4694
AbdoH7 <abdo.ah1102@gmail.com>
2023-06-26 06:19:08 +0300

    Added carriwave to handle files uploads

--- app/blueprint/user_blueprint.rb
+++ app/blueprint/user_blueprint.rb
@@ -1,5 +1,5 @@
 class UserBlueprint < Blueprinter::Base
     identifier :id
   
-    fields :first_name, :last_name, :email, :DOB, :user_type, :phone_number
+    fields :first_name, :last_name, :email, :DOB, :user_type, :phone_number, :avatar
   end
