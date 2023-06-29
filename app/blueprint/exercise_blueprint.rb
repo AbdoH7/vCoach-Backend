bec2dee1644c08c3af8d49138ffbd2f162bf07bc
AbdoH7 <abdo.ah1102@gmail.com>
2023-06-29 08:50:41 +0300

    Added image to exercis blueprint

--- app/blueprint/exercise_blueprint.rb
+++ app/blueprint/exercise_blueprint.rb
@@ -1,6 +1,6 @@
 class ExerciseBlueprint < Blueprinter::Base
     identifier :id
   
-    fields :name, :description, :model_available, :model_url, :video, :type, :body_area, :equipment, :instructions
+    fields :name, :description, :model_available, :model_url, :video, :type, :image, :body_area, :equipment, :instructions
   end
   
